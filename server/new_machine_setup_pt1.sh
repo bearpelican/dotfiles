83efb3d8b8af987e0371696b5beef587d11d8702
Andrew Shaw <andrew.t.shaw@gmail.com>
2019-03-13 12:42:25 -0700

    Updating ubuntu nvidia driver to 18.04

--- server/new_machine_setup_pt1.sh
+++ server/new_machine_setup_pt1.sh
@@ -38,8 +38,8 @@ sudo apt-get autoremove -y
 sudo apt-get autoclean -y
 sudo rm -rf /usr/local/cuda*
 
-sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
 
 sudo apt-get update  -y
 sudo apt-get -o Dpkg::Options::="--force-overwrite" install cuda cuda-drivers -y
